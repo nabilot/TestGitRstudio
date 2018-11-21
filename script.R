5c397c1a9600d2ca4d28b83dc9679345907874ea
Felipe Barros <felipe.b4rros@gmail.com>
2018-11-21 15:15:27 -0300

    Cambios en en plot

--- script.R
+++ script.R
@@ -1,2 +1,10 @@
+library(ggplot2)
 mtcars
-plot(mtcars$hp,mtcars$wt)
+
+ggplot(mtcars, aes(hp,wt)) +
+  geom_point()
+
+head(mtcars)
+ggplot(mtcars, aes(hp,drat)) +
+  geom_point() +
+  geom_smooth()
